cc9d9e46a1f31387f66c8ce2a86d8b7c03cc9a8e
mdelao18 <mdelao18@gmail.com>
2021-06-07 20:43:55 -0600

    Trabajo en clase # 9 Extraiga la lógica de almacenamiento en la BD fuera del Main.

--- src/com/ucreativa/vacunacion/Main.java
+++ src/com/ucreativa/vacunacion/Main.java
@@ -1,13 +1,11 @@
 package com.ucreativa.vacunacion;
 
 import com.ucreativa.vacunacion.entities.Amigo;
-import com.ucreativa.vacunacion.entities.BitacoraVacunas;
 import com.ucreativa.vacunacion.entities.Familiar;
 import com.ucreativa.vacunacion.entities.Persona;
+import com.ucreativa.vacunacion.repositories.InMemoryRepository;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -16,10 +14,11 @@ public class Main {
 
         Scanner in = new Scanner(System.in);
 
-        List<BitacoraVacunas> db = new ArrayList<>();
+        //List<BitacoraVacunas> db = new ArrayList<>();
+        InMemoryRepository repo = new InMemoryRepository();
 
         while (true) {
-            String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentezco, marca;
+            String nombre, cedula, edad, riesgo, isAmigo, relacion, facebook, parentezco, marca, print;
 
             Persona persona;
 
@@ -42,20 +41,23 @@ public class Main {
                 System.out.println("Facebook");
                 facebook = in.nextLine();
 
-                persona = new Amigo(nombre, cedula, Integer.parseInt(edad), riesgo.equals('S'), relacion, facebook);
+                persona = new Amigo(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), relacion, facebook);
 
             } else {
                 System.out.println("Parentezco");
                 parentezco = in.nextLine();
-                persona = new Familiar(nombre, cedula, Integer.parseInt(edad), riesgo.equals('S'), parentezco);
+                persona = new Familiar(nombre, cedula, Integer.parseInt(edad), riesgo.equals("S"), parentezco);
             }
             System.out.println("Vacuna -- Marca");
             marca = in.nextLine();
-
-            db.add(new BitacoraVacunas(persona, marca, new Date()));
-            String print = in.nextLine();
-            for (BitacoraVacunas item : db) {
-                System.out.println(item.getPersona().getNombre() + "  vacunado(a) el " + item.getFecha());
+            //db.add(new BitacoraVacunas(persona, marca, new Date()));
+            repo.save(persona, marca, new Date());
+            System.out.println("Quiere imprimir lista (S)");
+            print = in.nextLine();
+            if (print.equals("s")) {
+                for (String item : repo.get()) {
+                    System.out.println(item);
+                }
 
             }
 
